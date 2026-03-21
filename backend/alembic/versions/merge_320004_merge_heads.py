bfabe9e4e9ab132bfa452944546d24f50ca5690f
Isaries <leolove3very@gmail.com>
2026-03-22 00:29:44 +0800

    fix: merge multiple Alembic head revisions

---
+++ backend/alembic/versions/merge_320004_merge_heads.py
@@ -0,0 +1,19 @@
+"""Merge heads h09ck320002 and m50sf320003.
+
+Revision ID: merge320004
+Revises: h09ck320002, m50sf320003
+Create Date: 2026-03-22
+"""
+
+revision = "merge320004"
+down_revision = ("h09ck320002", "m50sf320003")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
