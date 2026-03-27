5f8613dd3ef11b42207347602cf94373f4aed181
Isaries <leolove3very@gmail.com>
2026-03-27 18:22:20 +0800

    fix: merge alembic heads a8b9c0d1e2f3 and p8q9r0s1t2u3

---
+++ backend/alembic/versions/merge_320005_merge_heads.py
@@ -0,0 +1,19 @@
+"""Merge heads a8b9c0d1e2f3 and p8q9r0s1t2u3.
+
+Revision ID: merge320005
+Revises: a8b9c0d1e2f3, p8q9r0s1t2u3
+Create Date: 2026-03-27
+"""
+
+revision = "merge320005"
+down_revision = ("a8b9c0d1e2f3", "p8q9r0s1t2u3")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
