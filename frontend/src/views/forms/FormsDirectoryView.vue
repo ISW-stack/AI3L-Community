d63b8cc5879cf66aade4b55d1a830ce4f0506aa0
Isaries <leolove3very@gmail.com>
2026-04-01 13:14:16 +0800

    style: improve forms directory private notice banner styling

--- frontend/src/views/forms/FormsDirectoryView.vue
+++ frontend/src/views/forms/FormsDirectoryView.vue
@@ -239,11 +239,11 @@ watch(page, fetchForms)
         <BaseButton>{{ t('formsDirectory.createForm') }}</BaseButton>
       </router-link>
     </div>
-    <div class="flex items-start gap-2 bg-yellow-300 px-3 py-2 rounded mb-6 w-full">
-      <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-500 shrink-0" viewBox="0 0 24 24" fill="currentColor">
+    <div class="flex items-center gap-2.5 bg-amber-50 border border-amber-200 px-4 py-3 rounded-lg mb-6">
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-amber-500 shrink-0" viewBox="0 0 24 24" fill="currentColor">
         <path fill-rule="evenodd" d="M12 1.5a5.25 5.25 0 0 0-5.25 5.25v3a3 3 0 0 0-3 3v6.75a3 3 0 0 0 3 3h10.5a3 3 0 0 0 3-3v-6.75a3 3 0 0 0-3-3v-3A5.25 5.25 0 0 0 12 1.5Zm3.75 8.25v-3a3.75 3.75 0 1 0-7.5 0v3h7.5Z" clip-rule="evenodd" />
       </svg>
-      <p class="text-base font-bold text-black">{{ t('formsDirectory.privateNotice') }}</p>
+      <p class="text-sm font-medium text-amber-800">{{ t('formsDirectory.privateNotice') }}</p>
     </div>
 
     <div class="mb-4">
